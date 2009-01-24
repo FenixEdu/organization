98c8e3bffe7b5cc9cf1a4a7ed5d482737f91e31d
Luis Cruz <luis.cruz@ist.utl.pt>
2009-01-24 01:00:18 +0000

    Completed import of People, Responsibilities, SubProjects and Acounting Units.

--- organization/src/module/organization/domain/Person.java
+++ organization/src/module/organization/domain/Person.java
@@ -1,11 +1,29 @@
 package module.organization.domain;
 
+import module.organization.domain.PartyType.PartyTypeBean;
 import myorg.domain.MyOrg;
-import myorg.domain.User;
 import pt.ist.fenixWebFramework.services.Service;
 import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
 
 public class Person extends Person_Base {
+
+    public static PartyType getPartyTypeInstance() {
+	final String type = Person.class.getName();
+	PartyType partyType = PartyType.readBy(type);
+	if (partyType == null) {
+	    synchronized (Person.class) {
+		partyType = PartyType.readBy(Person.class.getName());
+		if (partyType == null) {
+		    final PartyTypeBean partyTypeBean = new PartyTypeBean();
+		    partyTypeBean.setType(type);
+		    partyTypeBean.setName(new MultiLanguageString("Pessoa"));
+		    partyType = PartyType.create(partyTypeBean);
+		}
+	    }
+	}
+	return partyType;
+    }
+
 
     public Person(MultiLanguageString partyName, PartyType partyType) {
 	super();
