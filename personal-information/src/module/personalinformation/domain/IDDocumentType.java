b35f9bc8d7edbae7ff03bdb26799de6d99e28b3e
João Figueiredo <joao.figueiredo@ist.utl.pt>
2009-02-22 01:13:21 +0000

    - moved personal information related code from organization - organized domain into some extra packages

---
+++ personal-information/src/module/personalinformation/domain/IDDocumentType.java
@@ -0,0 +1,44 @@
+/*
+ * @(#)IDDocumentType.java
+ *
+ * Copyright 2009 Instituto Superior Tecnico
+ * Founding Authors: João Figueiredo, Luis Cruz, Paulo Abrantes, Susana Fernandes
+ * 
+ *      https://fenix-ashes.ist.utl.pt/
+ * 
+ *   This file is part of the Organization Module for the MyOrg web application.
+ *
+ *   The Organization Module is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.*
+ *
+ *   The Organization Module is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Organization Module. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package module.personalinformation.domain;
+
+public enum IDDocumentType {
+
+    IDENTITY_CARD,
+
+    PASSPORT,
+
+    NAVY_IDENTITY_CARD,
+
+    AIR_FORCE_IDENTITY_CARD,
+
+    MILITARY_IDENTITY_CARD;
+
+    public String getName() {
+	return name();
+    }
+
+}
---
+++ personal-information/src/module/personalinformation/domain/PersonalInformation.java
@@ -0,0 +1,9 @@
+package module.personalinformation.domain;
+
+public class PersonalInformation extends PersonalInformation_Base {
+    
+    public  PersonalInformation() {
+        super();
+    }
+    
+}
