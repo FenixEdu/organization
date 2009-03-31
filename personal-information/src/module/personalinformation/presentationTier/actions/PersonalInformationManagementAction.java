6b051d942c28cb7c9934027885d72b37a8b9c73e
João Figueiredo <joao.figueiredo@ist.utl.pt>
2009-03-31 20:52:26 +0000

    - initial action for personal information management

---
+++ personal-information/src/module/personalinformation/presentationTier/actions/PersonalInformationManagementAction.java
@@ -0,0 +1,73 @@
+/*
+ * @(#)PersonalInformationManagementAction.java
+ *
+ * Copyright 2009 Instituto Superior Tecnico
+ * Founding Authors: João Figueiredo, Luis Cruz, Paulo Abrantes, Susana Fernandes
+ * 
+ *      https://fenix-ashes.ist.utl.pt/
+ * 
+ *   This file is part of the Organization Module for the MyOrg web application.
+ *
+ *   The Organization Module is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.*
+ *
+ *   The Organization Module is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Organization Module. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package module.personalinformation.presentationTier.actions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import myorg.domain.RoleType;
+import myorg.domain.VirtualHost;
+import myorg.domain.contents.ActionNode;
+import myorg.domain.contents.Node;
+import myorg.domain.groups.Role;
+import myorg.presentationTier.actions.ContextBaseAction;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import pt.ist.fenixWebFramework.servlets.functionalities.CreateNodeAction;
+import pt.ist.fenixWebFramework.struts.annotations.Mapping;
+
+@Mapping(path = "/personalInformation")
+public class PersonalInformationManagementAction extends ContextBaseAction {
+
+    @CreateNodeAction(bundle = "PERSONAL_INFORMATION_RESOURCES", key = "add.node.manage.personal.information", groupKey = "label.module.personal.information")
+    public final ActionForward createOrganizationNode(final ActionMapping mapping, final ActionForm form,
+	    final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+
+	final VirtualHost virtualHost = getDomainObject(request, "virtualHostToManageId");
+	final Node parentOfNodes = getDomainObject(request, "parentOfNodesToManageId");
+
+	final ActionNode topActionNode = ActionNode.createActionNode(virtualHost, parentOfNodes, "/personalInformation", "intro",
+		"resources.PersonalInformationResources", "label.module.personal.information", Role.getRole(RoleType.MANAGER));
+
+	ActionNode.createActionNode(virtualHost, topActionNode, "/personalInformation", "manage",
+		"resources.PersonalInformationResources", "label.manage.information", Role.getRole(RoleType.MANAGER));
+
+	return forwardToMuneConfiguration(request, virtualHost, topActionNode);
+    }
+
+    public ActionForward intro(final ActionMapping mapping, final ActionForm form, final HttpServletRequest request,
+	    final HttpServletResponse response) throws Exception {
+	return forward(request, "/personalInformation/intro.jsp");
+    }
+
+    public ActionForward manage(final ActionMapping mapping, final ActionForm form, final HttpServletRequest request,
+	    final HttpServletResponse response) throws Exception {
+	return forward(request, "/personalInformation/manage.jsp");
+    }
+}
---
+++ personal-information/web/personalInformation/intro.jsp
@@ -0,0 +1,7 @@
+<%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
+<%@ taglib uri="/WEB-INF/struts-bean.tld" prefix="bean" %>
+
+<h2><bean:message key="label.module.personal.information" bundle="PERSONAL_INFORMATION_RESOURCES" /></h2>
+<ul>
+	<li><bean:message key="label.manage.information" bundle="PERSONAL_INFORMATION_RESOURCES"/></li>
+</ul>
